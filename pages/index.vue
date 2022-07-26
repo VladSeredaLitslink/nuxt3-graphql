1719e8eebe18ad193a5ea6caaf0ed5fe6cc34034
Vlad Sereda <v.u.sereda@gmail.com>
2022-07-26 09:47:11 +0300

    init project

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useQuery } from '@vue/apollo-composable'
+import { GET_CHARACTERS } from '@/api/queries'
+import { ElTable, ElTableColumn, ElContainer } from 'element-plus'
+
+
+const { loading, result } = useQuery(GET_CHARACTERS)
+</script>
+<template>
+  <el-container>
+    <div v-if="result">
+      <el-table :data="result.characters.results" style="width: 100%">
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="id" label="Id" width="180" />
+      </el-table>
+    </div>
+    <div v-if="loading">Loading...</div>
+  </el-container>
+</template>
