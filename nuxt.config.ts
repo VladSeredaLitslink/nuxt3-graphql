4dbeb739fadf65d34ac410e20c2ed0efe5b241cb
Vlad Sereda <v.u.sereda@gmail.com>
2022-07-28 14:25:24 +0300

    autofix nuxt-config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,17 +5,17 @@ export default defineNuxtConfig({
   head: {
     title: "nuxt3-GraphQL-pet",
     htmlAttrs: {
-      lang: "en",
+      lang: "en"
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
-    ],
+      { name: "format-detection", content: "telephone=no" }
+    ]
   },
   css: ["element-plus/dist/index.css"],
   build: {
-    transpile: ["@apollo/client", "ts-invariant/process"],
-  },
+    transpile: ["@apollo/client", "ts-invariant/process"]
+  }
 });
